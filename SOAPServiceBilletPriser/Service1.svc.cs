e79f07e9bf93a6fa31d7742d97a774b57cf34ba5
Linda Lene Schandorph Larsen <lind6181@edu.easj.dk>
2018-03-06 14:41:50 +0100

    Oprettet Billetpriser Bil og MC

--- SOAPServiceBilletPriser/Service1.svc.cs
+++ SOAPServiceBilletPriser/Service1.svc.cs
@@ -5,29 +5,25 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-
+using SOAPServiceBilletPriser;
+using BilletLibrary;
 namespace SOAPServiceBilletPriser
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        Bil priserBil = new Bil();
+        MC priserMC = new MC();
+
+        public int BilletPriserBil()
         {
-            return string.Format("You entered: {0}", value);
+            return priserBil.Pris();
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public int BilletPriserMC()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return priserMC.Pris();
         }
     }
 }
